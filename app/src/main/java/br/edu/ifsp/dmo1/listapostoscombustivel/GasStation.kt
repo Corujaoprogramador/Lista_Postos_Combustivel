f7c8234c7d97b359adf2e197b59b9bf6d36d2fdf
deivid <deivid9pereira@gmail.com>
2024-11-04 11:20:56 -0300

    Aula 04-11

---
+++ app/src/main/java/br/edu/ifsp/dmo1/listapostoscombustivel/GasStation.kt
@@ -0,0 +1,7 @@
+package br.edu.ifsp.dmo1.listapostoscombustivel
+
+class GasStation (val name: String,
+                  val address: String,
+                  val gas: Double,
+                  val etha: Double) {
+}
